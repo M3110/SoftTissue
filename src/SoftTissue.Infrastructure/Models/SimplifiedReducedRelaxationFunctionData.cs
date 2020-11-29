1f356918ac1d9b88a67e8457b9de71ae4ada50fe
M3110 <57352425+M3110@users.noreply.github.com>
2020-11-29 12:58:19 -0300

    Fixed SimplifiedReducedRelaxationFunctionData and added IndependentVariable property.

--- src/SoftTissue.Infrastructure/Models/SimplifiedReducedRelaxationFunctionData.cs
+++ src/SoftTissue.Infrastructure/Models/SimplifiedReducedRelaxationFunctionData.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SoftTissue.Infrastructure.Models
 {
     public class SimplifiedReducedRelaxationFunctionData
     {
-        public IEnumerable<double> VariableEList { get; set; }
+        public int NumberOfVariables => this.VariableEList.Count == this.RelaxationTimeList.Count 
+            ? this.VariableEList.Count
+            : throw new Exception($"Number of variables E: {this.VariableEList.Count} must be equals to number of relaxation times: {this.RelaxationTimeList.Count}.");
 
-        public IEnumerable<double> RelaxationTimeList { get; set; }
+        public double IndependentVariable { get; set; }
+
+        public List<double> VariableEList { get; set; }
+
+        public List<double> RelaxationTimeList { get; set; }
     }
 }
