0a42f5ab6a858482e51962d8f90d1bcb647f8c7f
M3110 <57352425+M3110@users.noreply.github.com>
2020-11-29 12:53:23 -0300

    Fixed CalculateStrainRequest summary.

--- src/SoftTissue.DataContract/LinearViscoelasticity/CalculateStrain/CalculateStrainRequest.cs
+++ src/SoftTissue.DataContract/LinearViscoelasticity/CalculateStrain/CalculateStrainRequest.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace SoftTissue.DataContract.LinearViscoelasticity.CalculateStrain
 {
     /// <summary>
-    /// It represents the essencial request content to CalculateStrain operation.
+    /// It represents the request content to CalculateStrain operation.
     /// </summary>
     public class CalculateStrainRequest : OperationRequestBase
     {
