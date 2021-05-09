4e6b627725f0dd06000c61c1556b9daf013dcc69
Bruno Silveira <57108288+Meello@users.noreply.github.com>
2021-05-09 16:35:21 -0300

    Changed operation AnalyzeResults to also consider when the stress increase and added enum StressDirection.

---
+++ src/SoftTissue.DataContract/Models/StressDirection.cs
@@ -0,0 +1,18 @@
+﻿namespace SoftTissue.DataContract.Models
+{
+    /// <summary>
+    /// It represents the direction of stress, indicating if it is increasing or decreasing.
+    /// </summary>
+    public enum StressDirection
+    {
+        /// <summary>
+        /// The stress increase during the ramp time.
+        /// </summary>
+        Increase = 1,
+
+        /// <summary>
+        /// The stress decrease after the ramp time.
+        /// </summary>
+        Decrease = 2
+    }
+}
