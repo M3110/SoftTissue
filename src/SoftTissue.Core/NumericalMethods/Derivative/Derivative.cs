40a9cfc77d2be1528d5aac7580484175be1a2fd7
M3110 <57352425+M3110@users.noreply.github.com>
2021-06-03 20:05:46 -0300

    Fixed variable name in method Calculate of class Derivative.

--- src/SoftTissue.Core/NumericalMethods/Derivative/Derivative.cs
+++ src/SoftTissue.Core/NumericalMethods/Derivative/Derivative.cs
@@ -5,24 +5,21 @@ namespace SoftTissue.Core.NumericalMethods.Derivative
 {
     public class Derivative : IDerivative
     {
-        // TODO: Melhorar o método de derivada utilizado, para que aumente a precisão do valor calculado.
-        // PROPOSTA: Usar um método de ordem maior.
-
         public double Calculate<TInput>(Func<TInput, double, double> Equation, TInput input, double time)
             where TInput : ViscoelasticModelInput
         {
-            double currentValue = Equation(input, time - input.TimeStep);
+            double previous = Equation(input, time - input.TimeStep);
             double nextValue = Equation(input, time + input.TimeStep);
 
-            return (nextValue - currentValue) / (2 * input.TimeStep);
+            return (nextValue - previous) / (2 * input.TimeStep);
         }
 
         public double Calculate(Func<double, double> Equation, double step, double time)
         {
-            double currentValue = Equation(time - step);
+            double previous = Equation(time - step);
             double nextValue = Equation(time + step);
 
-            return (nextValue - currentValue) / (2 * step);
+            return (nextValue - previous) / (2 * step);
         }
 
         public double Calculate(double initialPoint, double finalPoint, double step)
