82d74665ac18920e07baddb994efc85413ebe45b
M3110 <57352425+M3110@users.noreply.github.com>
2021-06-03 20:09:06 -0300

    Fixed summary at ICalculateResults.

--- src/SoftTissue.Core/Operations/ViscoelasticModel/CalculateResults/ICalculateResults.cs
+++ src/SoftTissue.Core/Operations/ViscoelasticModel/CalculateResults/ICalculateResults.cs
@@ -5,7 +5,7 @@ using SoftTissue.DataContract.ViscoelasticModel.CalculateResults;
 namespace SoftTissue.Core.Operations.ViscoelasticModel.CalculateResults
 {
     /// <summary>
-    /// It contains methods and parameters shared between operations to calculate a result.
+    /// It is responsible to calculate the results to a generic viscoelastic model.
     /// </summary>
     /// <typeparam name="TRequest"></typeparam>
     /// <typeparam name="TRequestData"></typeparam>
